6cdf20e06e7ede1d54b942d043da62e5bc06fe94
anton <a.kaprielov@adguard.com>
2019-07-25 13:16:40 +0300

    add resize editor

--- src/options/components/Editor/index.js
+++ src/options/components/Editor/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactResizeDetector from 'react-resize-detector';
 import AceEditor from 'react-ace';
 
 import 'brace/ext/searchbox';
@@ -13,17 +14,26 @@ function onChange(newValue) {
 }
 
 function Editor() {
+    const reactAceComponent = React.createRef();
+
+    function onResize() {
+        reactAceComponent.current.editor.resize();
+    }
+
     return (
         <div className="editor">
             <AceEditor
-                width="620px"
-                height="300px"
+                ref={reactAceComponent}
+                width="100%"
+                height="100%"
                 mode="adguard"
                 theme="textmate"
                 onChange={onChange}
                 name="user-filter"
+                showPrintMargin={false}
                 editorProps={{ $blockScrolling: true }}
             />
+            <ReactResizeDetector handleWidth handleHeight onResize={onResize} />
         </div>
     );
 }

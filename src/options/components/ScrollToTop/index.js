b4e03b461bd059ba36847d6fabc15f766b03dd61
anton <a.kaprielov@adguard.com>
2019-08-08 15:21:04 +0300

    refactor ScrollToTop

--- src/options/components/ScrollToTop/index.js
+++ src/options/components/ScrollToTop/index.js
@@ -1,12 +1,27 @@
-import { useEffect } from 'react';
+import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const ScrollToTop = ({ children, location: { pathname } }) => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+class ScrollToTop extends Component {
+    componentDidUpdate(prevProps) {
+        const { location } = this.props;
+        const { location: prevLocation } = prevProps;
+        if (location !== prevLocation) {
+            window.scrollTo(0, 0);
+        }
+    }
 
-    return children || null;
+    render() {
+        const { children } = this.props;
+        return children;
+    }
+}
+
+ScrollToTop.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    location: PropTypes.object.isRequired,
+    children: PropTypes.arrayOf(PropTypes.node).isRequired,
 };
+
 
 export default withRouter(ScrollToTop);

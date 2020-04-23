4168c509fab6c9da4681c587317ee7f4c75c4453
Maxim Topciu <maximtop@gmail.com>
2020-04-23 16:23:57 +0300

    AG-2050 apply eslint autofix

--- tasks/index.js
+++ tasks/index.js
@@ -17,31 +17,31 @@ import renewLocales from './renew-locales';
 import updateBuildInfo from './update-build-info';
 
 // download filters to repository
-export const downloadFilters = gulp.series(downloadAllFilters, updateLocalScriptRules, done => done());
+export const downloadFilters = gulp.series(downloadAllFilters, updateLocalScriptRules, (done) => done());
 
 // download localizations to repository
-export const updateLocalesStream = gulp.series(updateLocales, done => done());
+export const updateLocalesStream = gulp.series(updateLocales, (done) => done());
 
 // upload localizations to oneskyapp
-export const uploadLocalesStream = gulp.series(uploadLocales, done => done());
+export const uploadLocalesStream = gulp.series(uploadLocales, (done) => done());
 
 // tests
-export const runTests = gulp.series(tests, done => done());
+export const runTests = gulp.series(tests, (done) => done());
 
 // build updates files
-export const buildUpdatesFilesStream = gulp.series(buildUpdatesFiles, done => done());
+export const buildUpdatesFilesStream = gulp.series(buildUpdatesFiles, (done) => done());
 
 // dev build
-export const buildDev = gulp.series(chromium, firefoxAmo, firefoxWebext, edge, api, done => done());
+export const buildDev = gulp.series(chromium, firefoxAmo, firefoxWebext, edge, api, (done) => done());
 
 // beta build
-export const buildBeta = gulp.series(chromium, firefoxWebext, edge, api, updateBuildInfo, clean, done => done());
+export const buildBeta = gulp.series(chromium, firefoxWebext, edge, api, updateBuildInfo, clean, (done) => done());
 
 // release build
-export const buildRelease = gulp.series(chromium, opera, firefoxAmo, edge, updateBuildInfo, clean, done => done());
+export const buildRelease = gulp.series(chromium, opera, firefoxAmo, edge, updateBuildInfo, clean, (done) => done());
 
 // download resources
-export const downloadResources = gulp.series(downloadFilters, updatePublicSuffixList, done => done());
+export const downloadResources = gulp.series(downloadFilters, updatePublicSuffixList, (done) => done());
 
 // renew locales
 export const rebuildLocales = gulp.series(renewLocales);

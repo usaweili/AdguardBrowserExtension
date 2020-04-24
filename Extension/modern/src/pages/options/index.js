05ba6805aaad54c1dc5698120b3a332c192cc21c
Maxim Topciu <maximtop@gmail.com>
2020-04-24 17:13:58 +0300

    AG-2050 add todo notes

--- Extension/modern/src/pages/options/index.js
+++ Extension/modern/src/pages/options/index.js
@@ -8,6 +8,10 @@ import 'mobx-react/batchingForReactDom';
 
 document.title = i18n.translate('options_settings');
 
+// TODO
+//  1. remove service information from svg icons
+//  2. all components should be functional
+//  3. remove legacy options page, and associated files
 ReactDOM.render(
     <Provider>
         <App />

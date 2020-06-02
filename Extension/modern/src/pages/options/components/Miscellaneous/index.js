055905bd2d788c4f785851a730ffc5e69e0fa5cf
tvinzz <o.labudko@adguard.com>
2020-06-02 06:52:27 +0200

    Miscellaneous options page

--- Extension/modern/src/pages/options/components/Miscellaneous/index.js
+++ Extension/modern/src/pages/options/components/Miscellaneous/index.js
@@ -1,128 +1,120 @@
-import React, { Fragment, Component } from 'react';
+import React, {Fragment, useContext} from 'react';
+import {observer} from 'mobx-react';
 import SettingsSection from '../Settings/SettingsSection';
 import SettingsSet from '../Settings/SettingsSet';
 import Setting from '../Settings/Setting';
 import messenger from '../../../../services/messenger';
+import rootStore from "../../stores";
+import log from "../../../../services/log.js";
+import i18n from "../../../../services/i18n";
 
 const MISCELLANEOUS_META = {
     sets: {
         useOptimizedFilters: {
             id: 'useOptimizedFilters',
-            title: 'Use optimized filters',
-            description: 'Lists of filters optimized for mobile devices',
+            title: i18n.translate('options_use_optimized_filters'),
+            description: i18n.translate('options_use_optimized_filters_desc'),
             settings: ['useOptimizedFilters'],
         },
         integrationModeCheckbox: {
             id: 'integrationModeCheckbox',
-            title: 'Enable integration mode',
-            description: 'Learn more about it',
+            title: i18n.translate('options_disable_integration_mode'),
+            description: i18n.translate('options_learn_more'),
             settings: ['integrationModeCheckbox'],
         },
         enableHitsCount: {
             id: 'enableHitsCount',
-            title: 'Send statistics of ad filters usage',
-            description: 'Learn more about it',
+            title: i18n.translate('options_collect_hit_stats'),
+            description: i18n.translate('options_learn_more'),
             settings: ['enableHitsCount'],
         },
         enableShowContextMenu: {
             id: 'enableShowContextMenu',
-            title: 'Add AdGuard item to browser\'s context menu',
+            title: i18n.translate('options_show_context_menu'),
             settings: ['enableShowContextMenu'],
         },
         showInfoAboutAdguardFullVersion: {
             id: 'showInfoAboutAdguardFullVersion',
-            title: 'Show information on the AdGuard full version',
+            title: i18n.translate('options_show_adguard_full_version'),
             settings: ['showInfoAboutAdguardFullVersion'],
         },
         showAppUpdatedNotification: {
             id: 'showAppUpdatedNotification',
-            title: 'Notify about extension updates',
+            title: i18n.translate('options_show_app_updated_notification'),
             settings: ['showAppUpdatedNotification'],
         },
     },
     settings: {
         useOptimizedFilters: {
-            id: 'useOptimizedFilters',
+            id: 'use-optimized-filters',
             type: 'checkbox',
+            inverted: false,
         },
         integrationModeCheckbox: {
-            id: 'integrationModeCheckbox',
+            id: 'integration-mode-disabled',
             type: 'checkbox',
+            inverted: true
         },
         enableHitsCount: {
-            id: 'enableHitsCount',
+            id: 'hits-count-disabled',
             type: 'checkbox',
+            inverted: true
         },
         enableShowContextMenu: {
-            id: 'enableShowContextMenu',
+            id: 'context-menu-disabled',
             type: 'checkbox',
+            inverted: true
         },
         showInfoAboutAdguardFullVersion: {
-            id: 'showInfoAboutAdguardFullVersion',
+            id: 'show-info-about-adguard-disabled',
             type: 'checkbox',
+            inverted: true
         },
         showAppUpdatedNotification: {
-            id: 'showAppUpdatedNotification',
+            id: 'show-app-updated-disabled',
             type: 'checkbox',
+            inverted: true
         },
     },
 };
 
-class Miscellaneous extends Component {
-    state = {};
 
-    async componentDidMount() {
-        let settings;
-        const settingsIds = Object.keys(MISCELLANEOUS_META.settings);
-        try {
-            settings = await messenger.getSettingsByIds(settingsIds);
-        } catch (e) {
-            console.log(e);
-        }
-        this.setState({ settings });
+const Miscellaneous = observer(() => {
+    const {settingsStore} = useContext(rootStore);
+    const {settings} = settingsStore;
+
+    if (!settings) {
+        return null;
     }
 
-    handleSettingChange = async ({ id, data }) => {
-        try {
-            await messenger.updateSetting(id, data);
-            console.log(`Settings ${id} was changed to ${data}`);
-        } catch (e) {
-            console.log(e);
-            return;
-        }
-        this.setState((state) => {
-            const { settings } = state;
-            const setting = settings[id];
-            const updatedSetting = {
-                ...setting,
-                value: data,
-            };
-            return {
-                ...state,
-                settings: {
-                    ...settings,
-                    [id]: updatedSetting,
-                },
-            };
-        });
+    const settingChangeHandler = async ({id, data}) => {
+        log.info(`Setting ${id} set to ${data}`);
+        await settingsStore.updateSetting(id, data);
     };
 
-    renderSettings = (settingsIds) => {
-        const settingsData = this.state.settings;
-        const settingsMeta = settingsIds
-            .map(settingId => MISCELLANEOUS_META.settings[settingId]);
-        const enrichedSettings = settingsMeta
-            .map(settingsMetadata => ({
-                ...settingsMetadata,
-                ...settingsData[settingsMetadata.id],
-            }));
+    const renderSettings = (settingsIds) => {
+        const settingsMeta = settingsIds.map(settingId => MISCELLANEOUS_META.settings[settingId]);
+        const enrichedSettings = settingsMeta.map(settingMeta => {
+            const settingData = settings.values[settingMeta.id];
+            return {
+                ...settingMeta,
+                value: settingData
+            };
+        });
 
         return enrichedSettings.map(setting => (
-            <Setting key={setting.id} setting={setting} handler={this.handleSettingChange} />
+            <Setting
+                key={setting.id}
+                id={setting.id}
+                type={setting.type}
+                value={setting.value}
+                inverted={setting.inverted}
+                handler={settingChangeHandler}
+            />
         ));
     };
 
-    renderSets = () => {
+    const renderSets = () => {
         const setsOrder = [
             'useOptimizedFilters',
             'integrationModeCheckbox',
@@ -132,59 +124,65 @@ class Miscellaneous extends Component {
             'showAppUpdatedNotification',
         ];
         const sets = setsOrder.map(setId => MISCELLANEOUS_META.sets[setId]);
-        return sets.map(set => (
-            <SettingsSet key={set.id} {...set}>{this.renderSettings(set.settings)}</SettingsSet>
-        ));
+        return sets.map(set => {
+            const inverted = MISCELLANEOUS_META.settings[set.id].inverted;
+            const disabled = !settings.values[MISCELLANEOUS_META.settings[set.id].id];
+            return (
+                <SettingsSet
+                    key={set.id}
+                    {...set}
+                    disabled={inverted ? !disabled : disabled}
+                >
+                    {renderSettings(set.settings)}
+                </SettingsSet>
+        )});
     };
 
-    handleFilteringLogClick = async () => {
+    const handleFilteringLogClick = async () => {
         await messenger.openFilteringLog();
     };
 
-    handleResetStatisticsClick = async () => {
+    const handleResetStatisticsClick = async () => {
         await messenger.resetStatistics();
     };
 
-    handleOpenChangelog = () => {
+    const handleOpenChangelog = () => {
         // TODO [maximtop] replace link with tds
         window.open('https://github.com/AdguardTeam/AdguardBrowserExtension/releases', '_blank');
     };
 
-    render() {
-        const { settings } = this.state;
-        return (
-            <Fragment>
-                <h2 className="title">Miscellaneous</h2>
-                {settings
-                && (
-                    <SettingsSection>
-                        {this.renderSets()}
-                    </SettingsSection>
-                )}
-                <button
-                    type="button"
-                    className="button button--m button--green content__btn"
-                    onClick={this.handleFilteringLogClick}
-                >
-                    Filtering log
-                </button>
-                <button
-                    type="button"
-                    className="button button--m button--green-bd content__btn"
-                    onClick={this.handleResetStatisticsClick}
-                >
-                    Reset statistics
-                </button>
-                <button
-                    type="button"
-                    className="button button--m button--green-bd content__btn"
-                    onClick={this.handleOpenChangelog}
-                >
-                    Changelog
-                </button>
-            </Fragment>
-        );
-    }
-}
+    return (
+        <Fragment>
+            <h2 className="title">Miscellaneous</h2>
+            {settings
+            && (
+                <SettingsSection>
+                    {renderSets()}
+                </SettingsSection>
+            )}
+            <button
+                type="button"
+                className="button button--m button--green content__btn"
+                onClick={handleFilteringLogClick}
+            >
+                Filtering log
+            </button>
+            <button
+                type="button"
+                className="button button--m button--green-bd content__btn"
+                onClick={handleResetStatisticsClick}
+            >
+                Reset statistics
+            </button>
+            <button
+                type="button"
+                className="button button--m button--green-bd content__btn"
+                onClick={handleOpenChangelog}
+            >
+                Changelog
+            </button>
+        </Fragment>
+    );
+});
 
 export default Miscellaneous;
--- Extension/modern/src/pages/options/components/Settings/SettingsSet.js
+++ Extension/modern/src/pages/options/components/Settings/SettingsSet.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './settings.pcss';
 
 const SettingsSet = (props) => {
-    const { title, description, children } = props;
+    const { title, description, children, disabled } = props;
+    const settingInfoClassName = `setting__info${disabled ? ' disabled': ''}`;
     return (
         <div className="setting">
-            <div className="setting__info">
+            <div className={settingInfoClassName}>
                 <div className="setting__title">
                     {title}
                 </div>
                 { description
-                        && (
-                        <div className="setting__desc">
-                            {description}
-                        </div>
-                        )}
+                && (
+                    <div className="setting__desc">
+                        {description}
+                    </div>
+                )}
                 {children}
             </div>
         </div>
@@ -30,6 +32,7 @@ SettingsSet.propTypes = {
     title: PropTypes.string,
     description: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
     children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]).isRequired,
+    disabled: PropTypes.bool,
 };
 
 export default SettingsSet;
